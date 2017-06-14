1389af02147e15a53e3946220ba6738313c36210
chrismomdjian <chrismomdjian@gmail.com>
2017-06-14 16:56:08 -0700

    added a new color to nav and new font

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 include("data.php");
 
-$pageTitle = "Required Courses";
+$page_title = "Required Courses";
 $grade_points = 0;
 $total_units = 0.0;
 $gpa = 0.0;
 
+// Current GPA
+foreach($classes as $course){
+  if($course['status'] == "complete"){
+    $grade_points += $course['grade_points'];
+    $total_units += $course['units'];
+  }
+}
+
+$gpa = $grade_points / $total_units;
+$gpa = round($gpa, 2); // round to 2 decimal places
+
+
 if(isset($_GET['sort'])) {
   if($_GET['sort'] == "complete") {
-    $pageTitle = "Completed Courses";
+    $page_title = "Completed Courses";
   } elseif($_GET['sort'] == "incomplete") {
-    $pageTitle = "Incomplete Courses";
+    $page_title = "Incomplete Courses";
   }
 }
 ?>
@@ -18,7 +30,7 @@ if(isset($_GET['sort'])) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title><?php echo $pageTitle; ?></title>
+    <title><?php echo $page_title; ?></title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
   </head>
@@ -34,19 +46,8 @@ if(isset($_GET['sort'])) {
 
 
     <div class="wrapper">
-      <h2><?php echo $pageTitle; ?></h2>
-
-      <!-- Current GPA -->
-      <?php
-        foreach($classes as $course){
-          if($course['status'] == "complete"){
-            $grade_points += $course['grade_points'];
-            $total_units += $course['units'];
-          }
-        }
-
-        $gpa = $grade_points / $total_units;
-      ?>
+      <h2><?php echo $page_title; ?></h2>
+      <h2><?php echo "Current GPA: ", $gpa; ?></h2>
 
       <ul>
         <?php
