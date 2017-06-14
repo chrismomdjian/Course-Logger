06c575f5c4e2163b194260d9db2fad79fba4c5d1
chrismomdjian <chrismomdjian@gmail.com>
2017-06-14 00:47:12 -0700

    first commit

---
+++ data.php
@@ -0,0 +1,87 @@
+<?php
+//Courses needed for Web Development Certification
+$classes = [];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "HTML and CSS",
+  "number" => 260,
+  "status" => "complete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "PHP and MySQL",
+  "number" => 261,
+  "status" => "complete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "JavaScript and jQuery",
+  "number" => 262,
+  "status" => "complete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Web Development Project: Planning",
+  "number" => 264,
+  "status" => "complete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Web Development Project: Construction",
+  "number" => 265,
+  "status" => "incomplete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Web Development Project: Maintenance",
+  "number" => 266,
+  "status" => "incomplete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Introduction to Programming with Java",
+  "number" => 112,
+  "status" => "complete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Python Programming",
+  "number" => 151,
+  "status" => "complete"
+];
+
+$classes[] = [
+  "department" => "Art",
+  "title" => "Web Graphics",
+  "number" => 250,
+  "status" => "incomplete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Unix/Linux Operating System",
+  "number" => 172,
+  "status" => "incomplete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Introduction to Ecommerce/Ebusiness",
+  "number" => 255,
+  "status" => "incomplete"
+];
+
+$classes[] = [
+  "department" => "Computer Science",
+  "title" => "Introduction to Computer Networking",
+  "number" => 190,
+  "status" => "incomplete"
+];
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include("data.php");
+
+$pageTitle = "Required Courses";
+
+if(isset($_GET['sort'])) {
+  if($_GET['sort'] == "complete") {
+    $pageTitle = "Completed Courses";
+  } elseif($_GET['sort'] == "incomplete") {
+    $pageTitle = "Incomplete Courses";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo $pageTitle; ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <div class="main-nav">
+      <a href="http://chrismomdjian.com/PDO/index.php"><h4 id="nav-logo">Course Logger</h4></a>
+      <ul id="nav-items">
+        <a href="http://chrismomdjian.com/PDO/index.php?sort=complete"><li>Completed Courses</li></a>
+        <a href="http://chrismomdjian.com/PDO/index.php?sort=incomplete"><li>Unfinished Courses</li></a>
+      </ul>
+    </div>
+
+
+    <div class="wrapper">
+      <h2><?php echo $pageTitle; ?></h2>
+      <ul>
+        <?php
+        foreach($classes as $course) {
+          if(isset($_GET['sort'])) {
+            if($_GET['sort'] == $course['status']) {
+              echo "<li><p class='" . $course['status'] . "'>" . $course['title'] . "</p></li>";
+            }
+          } else {
+            echo "<li><p class='" . $course['status'] . "'>" . $course['title'] . "</li>";
+          }
+        }
+        ?>
+      </ul>
+    </div>
+  </body>
+</html>
