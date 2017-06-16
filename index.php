7825718291b076e1bb0d41a289a551518e163f25
chrismomdjian <chrismomdjian@gmail.com>
2017-06-15 23:51:44 -0700

    created links to each course which redirects to course.php containing course information

--- index.php
+++ index.php
@@ -1,50 +1,7 @@
 <?php
-include("data.php");
-
-$page_title = "Required Courses";
-$grade_points = 0;
-$total_units = 0.0;
-$gpa = 0.0;
-
-// Current GPA
-foreach($classes as $course){
-  if($course['status'] == "complete"){
-    $grade_points += $course['grade_points'];
-    $total_units += $course['units'];
-  }
-}
-
-$gpa = $grade_points / $total_units;
-$gpa = round($gpa, 2); // round to 2 decimal places
-
-
-if(isset($_GET['sort'])) {
-  if($_GET['sort'] == "complete") {
-    $page_title = "Completed Courses";
-  } elseif($_GET['sort'] == "incomplete") {
-    $page_title = "Incomplete Courses";
-  }
-}
+include("inc/data.php");
+include("inc/header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title><?php echo $page_title; ?></title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-
-  <body>
-    <div class="main-nav">
-      <a href="http://chrismomdjian.com/PDO/index.php"><h4 id="nav-logo">CourseLogger</h4></a>
-      <ul id="nav-items">
-        <a href="http://chrismomdjian.com/PDO/index.php?sort=complete"><li>Completed Courses</li></a>
-        <a href="http://chrismomdjian.com/PDO/index.php?sort=incomplete"><li>Unfinished Courses</li></a>
-      </ul>
-    </div>
 
 
     <div class="container">
@@ -54,22 +11,28 @@ if(isset($_GET['sort'])) {
         </div>
 
         <div class="col-xs-12 col-sm-6">
-          <h2><?php echo $page_title; ?></h2>
+          <h2><?php echo "<h1>" . $page_title . "</h1>"; ?></h2>
           <ul>
             <?php
-            foreach($classes as $course) {
+            foreach($classes as $key => $course) {
               if(isset($_GET['sort'])) {
                 if($_GET['sort'] == $course['status']) {
-                  echo "<li><p class='" . $course['status'] . "'>" . $course['title'] . "</p></li>";
+                  echo "<li>
+                  <a href='http://chrismomdjian.com/PDO/course.php?id=" . $key . "'
+                  <p class='" . $course['status'] . "'>" . $course['title'] . "</p>
+                  </li></a>";
                 }
               } else {
-                echo "<li><p class='" . $course['status'] . "'>" . $course['title'] . "</li>";
+                echo "<li>
+                <a href='http://chrismomdjian.com/PDO/course.php?id=" . $key . "'
+                <p class='" . $course['status'] . "'>" . $course['title'] . "</p>
+                </li></a>";
               }
             }
             ?>
           </ul>
         </div>
       </div> <!-- end row -->
-    </div>
+    </div> <!-- end container -->
   </body>
 </html>
